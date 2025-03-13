4c44e97d34b51b624defb15e3721954c5f276043
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2025-03-13 18:22:28 +0100

    user-edit(#8): création de la page de modification du profil + formulaire

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,21 +2,46 @@
 
 namespace App\Controller;
 
+use App\Form\UserEditType;
+use App\Service\UserService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 
 final class ProfileController extends AbstractController
 {
-    public function __construct(private Security $security)
+    public function __construct(private Security $security, private UserService $userService)
     {}
 
-    #[Route('/profile', name: 'profile')]
+    #[Route('/profile', name: 'profile', methods: ['GET', 'POST'])]
     public function index(): Response
     {
         return $this->render('profile/index.html.twig', [
             'user' => $this->security->getUser(),
+        ]);
+    }
+
+    #[Route('/profile/edit', name: 'profile.edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request): Response
+    {
+        $user = $this->security->getUser();
+
+        $form = $this->createForm(UserEditType::class, $user, [
+            'action' => $this->generateUrl('profile.edit'),
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $this->userService->update($user);
+
+            return $this->redirectToRoute('profile');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form,
         ]);
     }
 }
--- tests/Controller/ProfileControllerTest.php
+++ tests/Controller/ProfileControllerTest.php
@@ -3,11 +3,14 @@
 namespace App\Tests\Controller;
 
 use App\Repository\UserRepository;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProfileControllerTest extends WebTestCase
 {
+    use RefreshDatabaseTrait;
+
     private KernelBrowser $client;
 
     protected function setUp(): void
@@ -17,30 +20,66 @@ class ProfileControllerTest extends WebTestCase
         
     /**
      * testRouteProfileExistWhenUserLogged
+     * 
+     * @dataProvider getDataRouteProfile
      *
      * @return void
      */
-    public function testRouteProfileExistWhenUserLogged(): void
+    public function testRouteProfileExistWhenUserLogged(string $path): void
     {
         $userRepository = static::getContainer()->get(UserRepository::class);
         $testUser = $userRepository->findOneByUsername('admin');
         $this->client->loginUser($testUser);
 
-        $this->client->request('GET', '/profile');
+        $this->client->request('GET', $path);
 
         self::assertResponseIsSuccessful();
     }
-    
+
     /**
      * testRouteProfileExistWhenUserNotLogged
-     *
+     * 
+     * @dataProvider getDataRouteProfile
+     * 
      * @return void
      */
-    public function testRouteProfileExistWhenUserNotLogged(): void
+    public function testRouteProfileExistWhenUserNotLogged(string $path): void
     {
-        $this->client->request('GET', '/profile');
+        $this->client->request('GET', $path);
 
         self::assertResponseStatusCodeSame(302);
         self::assertResponseRedirects('/connexion');
+    }
+    
+    /**
+     * getDataRouteProfile
+     *
+     * @return array
+     */
+    public static function getDataRouteProfile(): array
+    {
+        return [
+            ['/profile'],
+            ['/profile/edit'],
+        ];
+    }
+
+    public function testEditProfile(): void
+    {
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testUser = $userRepository->findOneByUsername('admin');
+        $this->client->loginUser($testUser);
+
+        $this->client->request('GET', '/profile/edit');
+        $this->client->submitForm('Modifier', [
+            'user_edit[username]' => 'admin1',
+            'user_edit[email]' => 'admin1@domaine.com',
+        ]);
+
+        $userEdit = $userRepository->findOneByUsername('admin1');
+
+        self::assertResponseRedirects('/profile');
+        self::assertEquals("admin1", $userEdit->getUsername());
+        self::assertEquals("admin1@domaine.com", $userEdit->getEmail());
     }
 }
