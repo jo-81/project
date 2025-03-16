af868347c16d2c1916ad3871c9138cf7e7980630
GeoffroyCOL <geoffroy.colpart81@gmail.com>
2025-03-16 22:21:31 +0100

    user-single(#4) : création de la page qui affiche les informations d'un utilisateur

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class UserController extends AbstractController
 {
+    public function __construct(private UserRepository $userRepository)
+    {
+    }
+
     #[Route('/admin/users', name: 'user.list', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('user/index.html.twig');
+    }
+
+    #[Route('/admin/users/{id}', name: 'user.single', methods: ['GET'])]
+    public function single(int $id): Response
+    {
+        $user = $this->userRepository->find($id);
+        if (is_null($user)) {
+            $this->addFlash('danger', \sprintf("L'utilisateur avec id: %d n'existe pas", $id));
+
+            return $this->redirectToRoute('user.list');
+        }
+
+        return $this->render('user/single.html.twig', ['user' => $user]);
     }
 }
